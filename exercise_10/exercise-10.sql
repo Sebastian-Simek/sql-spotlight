672bae374668c153ccfb26ae2e2096bc3c027d38
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-02 15:47:45 -0700

    passed more tests

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -1,2 +1,8 @@
 -- find the category name of all inventory items that have never been rented
 -- Similar to exercise 9 with an additonal join
+SELECT name FROM inventory
+LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN film ON film.film_id = inventory.film_id
+LEFT JOIN film_category ON film_category.film_id = film.film_id
+LEFT JOIN category ON category.category_id = film_category.category_id
+WHERE rental.rental_id is NULL
--- exercise_13/exercise_13.sql
+++ exercise_13/exercise_13.sql
@@ -1,3 +1,13 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+SELECT country,
+AVG (amount)
+FROM country
+LEFT JOIN city on city.country_id = country.country_id
+LEFT JOIN address on address.city_id = city.city_id
+LEFT JOIN customer on customer.address_id = address.address_id
+LEFT JOIN payment on payment.customer_id = customer.customer_id
+GROUP BY country.country_id
+ORDER BY AVG DESC
+LIMIT 10 OFFSET 1
